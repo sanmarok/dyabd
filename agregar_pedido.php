1c1de14ff882a779a61c630102865bf615902b7c
AgustinLunardello <agustinlunardello030504@gmail.com>
2023-11-06 14:59:02 -0300

    agregar pedidos y clientes

--- agregar_pedido.php
+++ agregar_pedido.php
@@ -11,55 +11,56 @@ $database = "grupo1";
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Conexión a la base de datos fallida: " . $conn->connect_error);
+  die("Conexión a la base de datos fallida: " . $conn->connect_error);
 }
 
 $query = "SELECT * FROM clientes";
 $clientes = $conn->query($query);
-// $clientes = $resultado->fetch_assoc();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Gestor de base de datos</title>
 
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
+
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-<div class="wrapper">
-  <!-- Navbar -->
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-dark" style="margin-left: 0;">
-            <!-- Left navbar links -->
-            <ul class=" navbar-nav">
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="index.php" class="nav-link">Inicio</a>
-                </li>
-            </ul>
-        </nav>
+  <div class="wrapper">
+    <!-- Navbar -->
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-dark" style="margin-left: 0;">
+      <!-- Left navbar links -->
+      <ul class=" navbar-nav">
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="index.php" class="nav-link">Inicio</a>
+        </li>
+      </ul>
+    </nav>
 
 
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <p>
-              </p>
-            </a>
-      </nav>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <p>
+          </p>
+        </a>
+    </nav>
 
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -92,24 +93,29 @@ $clientes = $conn->query($query);
                 <div class="card-body">
                   <div class="form-group">
                     <label for="idCliente">Clientes</label>
-                    <select  class="form-control" name="idCliente"  id="idCliente" >
+                    <select class="form-control" name="idCliente" id="idCliente">
                       <option value="">Seleccione un cliente</option>
-                      <?php while($cliente = $clientes->fetch_assoc()){?>
-                        <option value="<?php echo $cliente['idCliente'];?>"><?php echo $cliente['nombres']."".$cliente['apellidos']?></option>
-                      <?php }?>
+                      <?php while ($cliente = $clientes->fetch_assoc()) { ?>
+                        <option value="<?php echo $cliente['idCliente']; ?>">
+                          <?php echo $cliente['nombres'] . "" . $cliente['apellidos'] ?>
+                        </option>
+                      <?php } ?>
                     </select>
                   </div>
                   <div class="form-group">
                     <label for="direccion_pedido">Direccion</label>
-                    <input type="text" class="form-control" name="direccion_pedido" id="direccion_pedido" placeholder="Ingrese la direccion del pedido">
+                    <input type="text" class="form-control" name="direccion_pedido" id="direccion_pedido"
+                      placeholder="Ingrese la direccion del pedido">
                   </div>
                   <div class="form-group">
                     <label for="fecha_pedido">Fecha</label>
-                    <input type="date" class="form-control" name="fecha_pedido" id="fecha_pedido" placeholder="Ingrese la fecha">
+                    <input type="date" class="form-control" name="fecha_pedido" id="fecha_pedido"
+                      placeholder="Ingrese la fecha">
                   </div>
                   <div class="form-group">
                     <label for="estado_pedido">Estado</label>
-                    <select type="list" class="form-control" name="estado_pedido" id="estado_pedido" placeholder="Ingrese el estado del pedido">
+                    <select type="list" class="form-control" name="estado_pedido" id="estado_pedido"
+                      placeholder="Ingrese el estado del pedido">
                       <option value="Pendiente">Pendiente</option>
                       <option value="En proceso">En proceso</option>
                       <option value="Entregado">Entregado</option>
@@ -127,8 +133,7 @@ $clientes = $conn->query($query);
                   <button type="submit" class="btn btn-primary" name="submit" id='submit'>Agregar</button>
                 </div>
                 <?php
-                if (isset($_POST['submit']))
-                {
+                if (isset($_POST['submit'])) {
                   $direccion_pedido = $_POST['direccion_pedido'];
                   $fecha_pedido = $_POST['fecha_pedido'];
                   $estado_pedido = $_POST['estado_pedido'];
@@ -137,34 +142,28 @@ $clientes = $conn->query($query);
 
                   $query = "INSERT INTO pedidos(direccion, fecha_pedido,estado,detalle,IdCliente) 
                   VALUES ('$direccion_pedido','$fecha_pedido','$estado_pedido','$detalle_pedido','$idCliente')";
-                  // $stmt = $conn->query($query);
-                  // $stmt->bindParam(":direccion_pedido",$_POST["direccion_pedido"],PDO::PARAM_STR);
-                  // $stmt->bindParam(":fecha_pedido",$_POST["fecha_pedido"],PDO::PARAM_STR);
-                  // $stmt->bindParam(":estado_pedido",$_POST["estado_pedido"],PDO::PARAM_STR);
-                  // $stmt->bindParam(":detalle_pedido",$_POST["detalle_pedido"],PDO::PARAM_STR);
-                  // $stmt->bindParam(":IdCliente",$_POST["idCliente"],PDO::PARAM_INT);
-                  $resultado = mysqli_query($conn,$query);
-
-                  if( $resultado)
-                  {
+                  $stmt = $conn->prepare($query);
+                  if ($stmt->execute()) {
                     echo '<script>
                       Swal.fire({
                         title: "Exito!",
                         text: "Se agrego exitosamente el cliente.",
                         icon: "success"
                       });
+                      window.location: 
                     </script>';
-                  }
-                  else
-                  {
+                  } else {
                     echo '<script>
                       Swal.fire({
                         title: "Error",
                         text: "Hubo un error al intentar agregar al cliente.",
                         icon: "error"
-                        });
-                    </script>';
+                      });
+                      </script>';
+                    header("Location: http://localhost/dyabd/index.php");
                   }
+                  $stmt->close();
+                  $conn->close();
                 }
                 ?>
               </form>
@@ -180,19 +179,20 @@ $clientes = $conn->query($query);
     <!-- Control sidebar content goes here -->
   </aside>
   <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- bs-custom-file-input -->
-<script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-<!-- Page specific script -->
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- bs-custom-file-input -->
+  <script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+  <!-- Page specific script -->
 </body>
+
 </html>
